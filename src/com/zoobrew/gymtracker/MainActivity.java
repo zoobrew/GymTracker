7415701a997237465f28b2d62378e6f846f369f2
Jared Corso <corsoj13@gmail.com>
2013-01-30 19:54:03 -0500

    Added beginnning support for SQLight

--- src/com/zoobrew/gymtracker/MainActivity.java
+++ src/com/zoobrew/gymtracker/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ListFragment;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
@@ -21,7 +22,11 @@ public class MainActivity extends FragmentActivity implements
 	 * current dropdown position.
 	 */
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-
+	
+	boolean mExternalStorageAvailable = false;
+	boolean mExternalStorageWriteable = false;
+	String state;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,6 +48,22 @@ public class MainActivity extends FragmentActivity implements
 								getString(R.string.title_section3), 
 								getString(R.string.title_section4),
 								getString(R.string.title_section5)}), this);
+		
+		
+		state = Environment.getExternalStorageState();
+
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+		    // We can read and write the media
+		    mExternalStorageAvailable = mExternalStorageWriteable = true;
+		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+		    // We can only read the media
+		    mExternalStorageAvailable = true;
+		    mExternalStorageWriteable = false;
+		} else {
+		    // Something else is wrong. It may be one of many other states, but all we need
+		    //  to know is we can neither read nor write
+		    mExternalStorageAvailable = mExternalStorageWriteable = false;
+		}
 	}
 	
 
@@ -86,25 +107,9 @@ public class MainActivity extends FragmentActivity implements
 		
 		return true;
 	}
-	/*@Override
-	public boolean onNavigationItemSelected(int position, long id) {
-		// When the given dropdown item is selected, show its contents in the
-		// container view.
-		ListFragment fragment = new DummySectionFragment();
-		
-		Bundle args = new Bundle();
-		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-		fragment.setArguments(args);
-		
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.container, fragment).commit();
-		return true;
-	}
-	*/
 
 	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
+	 * A fragment representing a selection of day's sets.
 	 */
 	public static class ExerciseSelectionFragment extends ListFragment {
 		/**
@@ -120,27 +125,6 @@ public class MainActivity extends FragmentActivity implements
 		 @Override
 	    public void onActivityCreated(Bundle savedInstanceState) {
 	        super.onActivityCreated(savedInstanceState);
-
-	        // Populate list with our static array of titles.
-	        //setListAdapter(new ArrayAdapter<String>(getActivity(),
-	                //android.R.layout.simple_list_item_activated_1, Shakespeare.TITLES));
-
-	        // Check to see if we have a frame in which to embed the details
-	        // fragment directly in the containing UI.
-	        //View detailsFrame = getActivity().findViewById(R.id.details);
-	        //mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
-
-	        //if (savedInstanceState != null) {
-	            // Restore last state for checked position.
-	          //  mCurCheckPosition = savedInstanceState.getInt("curChoice", 0);
-	        //}
-
-	        //if (mDualPane) {
-	            // In dual-pane mode, the list view highlights the selected item.
-	            //getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-	            // Make sure our UI is in the correct state.
-	            //showDetails(mCurCheckPosition);
-	        //}
 	    }
 		
 	    @Override
@@ -166,7 +150,7 @@ public class MainActivity extends FragmentActivity implements
 		    		Menu = res.getStringArray(R.array.menu_section5);
 		    		break;
 		    	default:
-		    		Menu = res.getStringArray(R.array.menu_section5);
+		    		Menu = res.getStringArray(R.array.menu_section1);
 		    		break;
 		    }
 		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
@@ -177,13 +161,7 @@ public class MainActivity extends FragmentActivity implements
 		/*@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			// Create a new TextView and set its text to the fragment's section
-			// number argument value.
-			TextView textView = new TextView(getActivity());
-			textView.setGravity(Gravity.CENTER);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return textView;
+			
 		}
 		*/
 
@@ -192,6 +170,8 @@ public class MainActivity extends FragmentActivity implements
 			  // Do something with the data
 			  Intent intent = new Intent(getActivity(), ExerciseDetailActivity.class);
 			  intent.putExtra(ARG_EXCERCISE_NUMBER, position);
+			  int number = getArguments().getInt(ARG_SECTION_NUMBER);
+			  intent.putExtra(ARG_SECTION_NUMBER, number);
 			  startActivity(intent);
 
 		  }
